cc4a5f9f4b2b0a773e292bbcb138de9563c967bf
Zeno Hui <zph111@mail.ustc.edu.cn>
2024-12-29 23:15:20 +0800

    集成jupiter

--- instock/bin/run_jupiter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-jupyter notebook --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token=stock_juipter --notebook-dir='/data'
-
-echo ------jupiter服务已启动 请不要关闭------
-echo 访问地址 : http://localhost:8888/
